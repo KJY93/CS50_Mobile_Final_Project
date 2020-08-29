224284e0f6ab0072bcc3052809ff303e21f6473b
JY <jiayue93@gmail.com>
2020-08-29 13:13:54 +0800

    added meal categories reducer

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
-
 import {
     FETCH_RANDOM_MEALS_BEGIN,
     FETCH_RANDOM_MEALS_SUCCESS,
     FETCH_RANDOM_MEALS_FAILURE,
-    FETCH_SEARCH_MEAL_BY_NAME_BEGIN, 
-    FETCH_SEARCH_MEAL_BY_NAME_SUCCESS, 
-    FETCH_SEARCH_MEAL_BY_NAME_FAILURE, 
+    FETCH_SEARCH_MEAL_BY_NAME_BEGIN,
+    FETCH_SEARCH_MEAL_BY_NAME_SUCCESS,
+    FETCH_SEARCH_MEAL_BY_NAME_FAILURE,
+    FETCH_MEALS_CATEGORIES_BEGIN,
+    FETCH_MEALS_CATEGORIES_SUCCESS,
+    FETCH_MEALS_CATEGORIES_FAILURE,
 }
     from './actions';
 
@@ -61,16 +63,16 @@ const searchMealByNameReducer = (state = initialSearchRecipeState, action) => {
             return {
                 ...state,
                 loading: true,
-                error: null,           
+                error: null,
             }
-        
+
         case FETCH_SEARCH_MEAL_BY_NAME_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 meals: action.payload,
             }
-        
+
         case FETCH_SEARCH_MEAL_BY_NAME_FAILURE:
             return {
                 ...state,
@@ -78,7 +80,43 @@ const searchMealByNameReducer = (state = initialSearchRecipeState, action) => {
                 error: action.payload,
                 meals: null,
             }
-        
+
+        default:
+            return state;
+    }
+}
+
+// Initial state for recipes categories
+const initialMealCategoriesState = {
+    category: null,
+    loading: false,
+    error: null,
+}
+
+const mealCategoriesReducer = (state = initialMealCategoriesState, action) => {
+    switch (action.type) {
+        case FETCH_MEALS_CATEGORIES_BEGIN:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            }
+
+        case FETCH_MEALS_CATEGORIES_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                category: action.payload,
+            }
+
+        case FETCH_MEALS_CATEGORIES_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                category: null,
+            }
+
         default:
             return state;
     }
@@ -88,6 +126,7 @@ const searchMealByNameReducer = (state = initialSearchRecipeState, action) => {
 const reducer = combineReducers({
     random_meal: randomMealReducer,
     search_meal: searchMealByNameReducer,
+    meal_category: mealCategoriesReducer,
 });
 
 export default reducer
