5a63c16cb47574acf69811def742b83d1e48e8a8
JY <jiayue93@gmail.com>
2020-08-29 13:29:06 +0800

    added in PropTypes to validate incoming parameters type and also added styling to text display

--- screens/RecipeDetails.js
+++ screens/RecipeDetails.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { getRecipeById } from '../services/api'
 import Recipe from './Recipe'
 
 class RecipeDetailsScreen extends React.Component {
+    static propTypes = {
+        route: PropTypes.shape({
+            params: PropTypes.shape({
+                id: PropTypes.string,
+                recipeName: PropTypes.string,
+            })
+        })
+    }
+
     state = {
         meals: null,
         id: this.props.route.params.id
@@ -19,8 +28,16 @@ class RecipeDetailsScreen extends React.Component {
         if (!this.state.id) return <Text>An error has occured</Text>
         return this.state.meals
             ? <Recipe {...this.state.meals} />
-            : <Text>Loading...</Text>
+            : <View style={styles.textContainer}><Text>Loading...</Text></View>
     }
 }
+
+const styles = StyleSheet.create({
+    textContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
 
 export default RecipeDetailsScreen;
